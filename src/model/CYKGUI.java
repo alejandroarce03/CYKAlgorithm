c4f620fc49c6e21431ba47b01614286c060d37ad
Alejandro <alejandrocutu1@gmail.com>
2021-11-15 22:11:35 -0500

    Add intructions on the UI

--- src/model/CYKGUI.java
+++ src/model/CYKGUI.java
@@ -54,7 +54,7 @@ public class CYKGUI {
 
 	}
 
-	@SuppressWarnings("null")
+	
 	@FXML
 	void enterBTN(ActionEvent event) {
 		init();
@@ -63,6 +63,73 @@ public class CYKGUI {
 		try {
 			w = wTXT.getText();
 			gram = grammaticalTXT.getText();
+			
+			String[][] matrixGram;
+			int numColumns =0;
+			if(!w.equals("") && !gram.equals("")) {
+
+
+				String[] rows = gram.split(",");
+
+				for(int i=0;i<rows.length;i++) {
+					if(numColumns<rows[i].length()) {
+						numColumns=rows[i].length();
+					}
+				}
+				matrixGram = new String[rows.length][numColumns-6];
+
+				for(int i=0;i<rows.length;i++) {
+
+					String[] p1 = rows[i].split("-->");
+
+					matrixGram[i][0]=p1[0].trim();
+
+					boolean proof=false;
+					String[] p2 = p1[1].split("\\|");
+					int x=0;
+					for(int j=1;j<p2.length;j++) {		
+
+						proof=true;
+
+						matrixGram[i][j] = p2[j-1].trim();
+
+						x=j;
+					}
+
+					if(p2.length>=2) {
+						matrixGram[i][x+1] = p2[x].trim();
+
+					}
+
+					if(!proof) {
+
+						matrixGram[i][1]=p1[1].trim();
+
+					}
+
+					
+				}
+				for (int i = 0; i < matrixGram.length; i++) {
+					for (int j = 0; j < matrixGram[i].length; j++) {
+						if(matrixGram[i][j]==null) {
+							matrixGram[i][j]="";
+						}
+						System.out.print(matrixGram[i][j] + " ");
+					}
+					System.out.println();
+				}	
+				
+			
+				cyk = new AlgorithmCYK(matrixGram,w);
+				boolean result = cyk.generateCYKMatrix();
+				if(result) {
+					trueLAB.setVisible(true);
+				}else {
+					falseLAB.setVisible(true);
+				
+				
+				}
+			}
 		}catch(Exception e) {
 			Alert alert = new Alert(Alert.AlertType.ERROR);
 			alert.setHeaderText(null);
@@ -72,73 +139,10 @@ public class CYKGUI {
 		}
 
 
-		String[][] matrixGram;
-		int numColumns =0;
-		if(!w.equals("") && !gram.equals("")) {
-
-
-			String[] rows = gram.split(",");
-
-			for(int i=0;i<rows.length;i++) {
-				if(numColumns<rows[i].length()) {
-					numColumns=rows[i].length();
-				}
-			}
-			matrixGram = new String[rows.length][numColumns-6];
-
-			for(int i=0;i<rows.length;i++) {
-
-				String[] p1 = rows[i].split("-->");
-
-				matrixGram[i][0]=p1[0].trim();
-
-				boolean proof=false;
-				String[] p2 = p1[1].split("\\|");
-				int x=0;
-				for(int j=1;j<p2.length;j++) {		
-
-					proof=true;
-
-					matrixGram[i][j] = p2[j-1].trim();
-
-					x=j;
-				}
-
-				if(p2.length>=2) {
-					matrixGram[i][x+1] = p2[x].trim();
-
-				}
-
-				if(!proof) {
-
-					matrixGram[i][1]=p1[1].trim();
-
-				}
-
-				
-			}
-			for (int i = 0; i < matrixGram.length; i++) {
-				for (int j = 0; j < matrixGram[i].length; j++) {
-					if(matrixGram[i][j]==null) {
-						matrixGram[i][j]="";
-					}
-					System.out.print(matrixGram[i][j] + " ");
-				}
-				System.out.println();
-			}	
-			
-			
-			cyk = new AlgorithmCYK(matrixGram,w);
-			boolean result = cyk.generateCYKMatrix();
-			if(result) {
-				trueLAB.setVisible(true);
-			}else {
-				falseLAB.setVisible(true);
-			}
-			
+		
 
 			
 			
-		}
+		
 	}
 }
